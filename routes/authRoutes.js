87ac683b6fe6fcdda4a31ce9dfbd2fd5370c545c
Shahd <shahd.kaddoura182@gmail.com>
2026-04-15 19:06:33 +0300

    feat(auth): add JWT middleware and protected auth route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const { register, login } = require("../controllers/authController");
+const { protect } = require("../middleware/authMiddleware");
+
+router.get("/me", protect, (req, res) => {
+  res.json({
+    message: "Protected route works",
+    user: req.user,
+  });
+});
 
 router.post("/register", register);
 router.post("/login", login);
